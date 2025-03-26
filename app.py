6e67bcc7d4e1418e96730e22b817efc5eb29097a
Krzysztof Smykała <ksmykala@qzsolutions.pl>
2025-03-26 07:35:46 +0100

    Add JWT package

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -45,6 +47,9 @@ def create_app(db_url=None):
     db.init_app(app)
 
     api = Api(app)
+
+    app.config['JWT_SECRET'] == os.getenv('JWT_SECRET')
+    jwt = JWTManager(app)
 
     with app.app_context():
         db.create_all()
