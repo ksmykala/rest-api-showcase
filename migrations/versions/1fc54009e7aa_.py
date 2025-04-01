8e83dcb3372379b20a95a98df3aa92da9621e2fd
Krzysztof Smykała <ksmykala@qzsolutions.pl>
2025-04-01 09:39:58 +0200

    Add user email to the model

--- migrations/versions/1fc54009e7aa_.py
+++ migrations/versions/1fc54009e7aa_.py
@@ -1,8 +1,8 @@
 """Add email column to users
 
-Revision ID: 190b3e4a92ed
+Revision ID: 1fc54009e7aa
 Revises: e04cb14d073f
-Create Date: 2025-04-01 07:26:16.970468
+Create Date: 2025-04-01 07:37:34.507623
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '190b3e4a92ed'
+revision = '1fc54009e7aa'
 down_revision = 'e04cb14d073f'
 branch_labels = None
 depends_on = None
@@ -18,7 +18,7 @@ depends_on = None
 
 def upgrade():
     with op.batch_alter_table('users', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('email', sa.String(), nullable=True))
+        batch_op.add_column(sa.Column('email', sa.String(), nullable=False))
         batch_op.create_unique_constraint('email', ['email'])
 
 
